389b750e7498b85fb3c5696f806a75fc8d4d7bfd
John Toniutti <john.toniutti@gmail.com>
2024-10-24 21:14:34 +0200

    Add messages docs

--- pyown/messages/ack.py
+++ pyown/messages/ack.py
@@ -11,9 +11,10 @@ __all__ = [
 
 class ACK(BaseMessage):
     """
-    Represent an ACK message
+    Represents an ACK message.
+    Used to acknowledge a command sent or to end a list of messages sent as a result of a command.
 
-    Syntax: *#*1##
+    Syntax: `*#*1##` (constant string)
     """
     _type: MessageType = MessageType.ACK
     _tags: tuple[str, str] = ("#", "1")
@@ -25,11 +26,19 @@ class ACK(BaseMessage):
 
     @property
     def message(self) -> str:
+        """
+        Returns the string representation of the message.
+
+        It's a constant string: `*#*1##`
+        """
         return "*#*1##"
 
     @classmethod
     def parse(cls, tags: list[str]) -> Self:
-        """Parse the tags of a message from the OpenWebNet bus."""
+        """
+        Parses the tags of a message from the OpenWebNet bus.
+        In this case, it only checks if the tags are correct.
+        """
         # the first tag bust be #
         if tags[0] != "#" and tags[1] != "1":
             raise InvalidMessage(message="Invalid ACK message")
--- pyown/messages/base.py
+++ pyown/messages/base.py
@@ -4,10 +4,11 @@ import abc
 import copy
 import re
 from enum import StrEnum
-from typing import Final, Pattern, Self, Any
+from typing import Final, Pattern, Self, Any, Sequence
 
-from ..exceptions import InvalidMessage
+from ..exceptions import InvalidMessage, InvalidTag
 from ..tags import Who, Where
+from ..tags.base import is_valid_tag
 
 __all__ = [
     "MessageType",
@@ -29,6 +30,16 @@ class MessageType(StrEnum):
 
 
 class BaseMessage(abc.ABC):
+    """
+    Base class for all the messages from the OpenWebNet bus.
+
+    It defines the structure of the messages and the methods to parse and create them.
+
+    Attributes:
+        prefix (str): Prefix of the message
+        suffix (str): Suffix of the message
+        separator (str): Separator of the tags
+    """
     _type: MessageType = MessageType.GENERIC  # Type of the message
     _tags: Any  # Contains the tags of the message
 
@@ -43,19 +54,9 @@ class BaseMessage(abc.ABC):
         raise NotImplementedError
 
     def __str__(self) -> str:
-        """
-        Return the message as a string.
-        Returns:
-            str: The message
-        """
         return self.message
 
     def __repr__(self) -> str:
-        """
-        Return the representation of the message.
-        Returns:
-            str: The representation
-        """
         return f"<{self.__class__.__name__}: {','.join([str(tag) for tag in self._tags])}>"
 
     def __hash__(self) -> int:
@@ -72,28 +73,30 @@ class BaseMessage(abc.ABC):
     @classmethod
     def pattern(cls) -> Pattern[str]:
         """
-        Return the regex pattern used to match the message represented by the class.
+        Returns the regex pattern used to match the message represented by the class.
         Returns:
-            Pattern[str]: The regex pattern
+            Pattern[str]: The regex pattern used to match the message
         """
         return cls._regex
 
     @property
     def tags(self) -> tuple[str] | list[str]:
         """
-        Return the tags of the message.
+        Returns a copy of the tags.
+
         The tags are the elements that compose the message, like the WHO, WHAT, WHERE, etc.
 
         Returns:
-            list[StrEnum, str]: The tags of the message
+            tuple[str] | list[str]: The tags of the message
         """
-        # Return a new copy of the tags to avoid modifications
+        # Returns a new copy of the tags to avoid modifications
         return copy.deepcopy(self._tags)
 
     @property
     def type(self) -> MessageType:
         """
-        Return the type of the message.
+        Returns the type of the message.
+
         Returns:
             MessageType: The type of the message
         """
@@ -103,7 +106,10 @@ class BaseMessage(abc.ABC):
     @abc.abstractmethod
     def message(self) -> str:
         """
-        Return the message represented by the class.
+        Returns the message represented by the class.
+
+        This function is equivalent to the `__str__` method.
+
         Returns:
             str: The message
         """
@@ -111,48 +117,86 @@ class BaseMessage(abc.ABC):
 
     @property
     def bytes(self) -> bytes:
+        """
+        Returns the message encoded in bytes.
+
+        Returns:
+            bytes: The message encoded in bytes
+        """
         return self.message.encode("ascii")
 
     @classmethod
     @abc.abstractmethod
     def parse(cls, tags: list[str]) -> BaseMessage:
-        """Parse the tags of a message from the OpenWebNet bus."""
+        """
+        Parses the tags of a message and returns an instance of the class.
+        """
         raise NotImplementedError
 
     @property
     def who(self) -> Who | None:
-        """Return the WHO tag of the message."""
+        """
+        Returns the WHO tag of the message.
+        """
         return None
 
     @property
     def where(self) -> Where | None:
-        """Return the WHAT tag of the message."""
+        """
+        Returns the WHERE tag of the message.
+        """
         return None
 
 
 class GenericMessage(BaseMessage):
+    """
+    Represents a generic message.
+
+    This is used when the message has an unknown structure, or to send specific messages that
+    don't need a specific class to represent them.
+    """
     def __init__(self, tags: list[str]):
         self._tags = tags
 
     @property
     def message(self) -> str:
+        """
+        Returns the string representation of the message.
+
+        Returns:
+            str: The message
+        """
         return f"*{'*'.join(self._tags)}##"
 
     @classmethod
     def parse(cls, tags: list[str]) -> Self:
+        """
+        Parses the tags of a message from the OpenWebNet bus.
+        In this case, it only checks if the tags are correct.
+
+        Args:
+            tags: The tags of the message
+
+        Returns:
+            GenericMessage: The instance of the class
+        """
+        for tag in tags:
+            if not is_valid_tag(tag):
+                raise InvalidTag(tag)
+
         return cls(tags=tags)
 
 
 def parse_message(message: str) -> BaseMessage:
     """
-    Parse a message from the OpenWebNet bus.
+    Parses a message from the OpenWebNet bus.
 
     Args:
         message (str): The message to parse
 
     Returns:
         BaseMessage: The appropriate message class instance,
-        GenericMessage if the message has an unknown WHO tag or its structure is unknown
+            GenericMessage if the message has an unknown WHO tag or its structure is unknown
     """
     if message.count(BaseMessage.suffix) != 1:
         raise InvalidMessage(message=message)
